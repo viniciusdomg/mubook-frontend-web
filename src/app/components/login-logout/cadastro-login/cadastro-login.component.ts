deefc040fa723f346ca597e1c08b8b0895bdabb4
Vinicius de Oliveira <130383526+viniciusdomg@users.noreply.github.com>
2025-04-22 17:12:31 -0300

    wip: cria componentes de login e cadastro de login

---
+++ src/app/components/login-logout/cadastro-login/cadastro-login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-login',
+  imports: [],
+  templateUrl: './cadastro-login.component.html',
+  styleUrl: './cadastro-login.component.css'
+})
+export class CadastroLoginComponent {
+
+}
