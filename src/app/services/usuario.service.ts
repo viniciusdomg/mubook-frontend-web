ff171edd2cd362311bff68996c4d6b1afde7af4b
Vinicius de Oliveira <130383526+viniciusdomg@users.noreply.github.com>
2025-04-23 21:14:11 -0300

    feat: cria método para cadastro do usuário
    - Adiciona service com todos os serviços disponíveis de um usuário

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UsuarioModel } from '../models/usuario.model';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class UsuarioService {
+  private apiUrl = 'http://localhost:3000/usuarios';
+
+  constructor(private http: HttpClient) {}
+
+  todosUsuarios(): Observable<UsuarioModel[]>{
+    return this.http.get<UsuarioModel[]>(`${this.apiUrl}`);
+  }
+
+  cadastrar(usuario: UsuarioModel): Observable<UsuarioModel> {
+    return this.http.post<UsuarioModel>(`${this.apiUrl}`, { usuario });
+  }
+
+  buscarPorId(id: number): Observable<UsuarioModel> {
+    return this.http.get<UsuarioModel>(`${this.apiUrl}/${id}`);
+  }
+
+  atualizar(usuario: UsuarioModel): Observable<UsuarioModel> {
+    return this.http.put<UsuarioModel>(`${this.apiUrl}/${usuario.id}`, { usuario });
+  }
+
+  trocarSenha(id: number, novaSenha: string): Observable<any> {
+    return this.http.patch(`${this.apiUrl}/${id}/trocar_senha`, { senha: novaSenha });
+  }
+
+  deletar(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
