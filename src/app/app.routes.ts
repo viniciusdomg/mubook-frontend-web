573ee41224a5a7e27279f3c02c81b472d139eb27
Vinicius de Oliveira <130383526+viniciusdomg@users.noreply.github.com>
2025-04-23 21:15:50 -0300

    feat: nova tela home do site
    - Cria rota para tela home
    - Adiciona os componentes iniciais do home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from './components/auth/login/login.component';
 import {CadastroLoginComponent} from './components/auth/cadastro-login/cadastro-login.component';
+import {HomeComponent} from './home/home.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'fazer_cadastro',
     component: CadastroLoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   }
 ];
