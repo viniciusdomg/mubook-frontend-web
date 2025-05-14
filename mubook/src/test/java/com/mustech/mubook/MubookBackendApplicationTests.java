0586054b497518134b80fbd846cd89951f8cf89b
Vinicius de Oliveira <130383526+viniciusdomg@users.noreply.github.com>
2025-05-14 16:27:36 -0300

    build: Adicionando configurações Spring

---
+++ mubook/src/test/java/com/mustech/mubook/MubookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustech.mubook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MubookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
