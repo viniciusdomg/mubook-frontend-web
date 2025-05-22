ba67e614269ab2e8c1be003805efa440a514a185
Brandaumm <nathan.victor.brandao@gmail.com>
2025-05-22 19:02:06 -0300

    Sidebar e header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainBackgroundComponent } from './components/main-background/main-background.component';
-import { UserManagementComponent } from './components/user-management/user-management.component';
 @Component({
   selector: 'app-root',
   imports: [
     SidebarComponent,
     HeaderComponent,
     MainBackgroundComponent,
-    UserManagementComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
