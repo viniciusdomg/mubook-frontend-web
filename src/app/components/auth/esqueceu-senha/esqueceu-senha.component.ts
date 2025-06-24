520b7d0148140f5c9dd13cb9993b44e02c680803
viniciusdomg <viniciusmgomes2005@hotmail.com>
2025-06-24 11:29:32 -0300

    refactor: removendo partes não usadas no código

--- src/app/components/auth/esqueceu-senha/esqueceu-senha.component.ts
+++ src/app/components/auth/esqueceu-senha/esqueceu-senha.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from "@angular/forms";
-import { Router, RouterLink } from '@angular/router';
-import { NgClass } from '@angular/common';
+import { RouterLink } from '@angular/router';
+// import { NgClass } from '@angular/common';
 import { UsuarioModel } from '../../../models/usuario.model';
 import { UsuarioService } from '../../../services/usuario.service';
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
@@ -13,7 +13,7 @@ import Swal from 'sweetalert2';
   imports: [
     FormsModule,
     RouterLink,
-    NgClass,
+    // NgClass,
     SweetAlert2Module
   ],
   templateUrl: './esqueceu-senha.component.html',
@@ -26,22 +26,20 @@ export class EsqueceuSenhaComponent {
     email: '',
   };
 
-  senhaVisivel = false;
-
   constructor(
     private usuarioService: UsuarioService,
-    private router: Router
+    // private router: Router
   ) {}
 
   enviarEmailRedefinicaoSenha() {
     if (!this.usuarioModel.email) {
-      Swal.fire('Erro', 'Informe um e-mail válido.', 'error');
+      void Swal.fire('Erro', 'Informe um e-mail válido.', 'error');
       return;
     }
 
     this.usuarioService.solicitarRedefinicaoSenha(this.usuarioModel.email).subscribe({
       next: () => {
-        Swal.fire({
+        void Swal.fire({
             title: 'Sucesso',
             text: 'Instruções de redefinição de senha enviadas para o e-mail.',
             icon: 'success',
@@ -51,7 +49,7 @@ export class EsqueceuSenhaComponent {
       },
       error: (error) => {
         console.error(error);
-        Swal.fire('Erro', 'Não foi possível enviar o e-mail. Verifique o e-mail informado.', 'error');
+        void Swal.fire('Erro', 'Não foi possível enviar o e-mail. Verifique o e-mail informado.', 'error');
       }
     });
   }
