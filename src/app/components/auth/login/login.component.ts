11843d47f07229ea276b9463b15255511bb680aa
viniciusdomg <viniciusmgomes2005@hotmail.com>
2025-06-26 21:44:30 -0300

    feat: corrige layout da navbar e upload da foto do usuário

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {Router, RouterLink} from '@angular/router';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../../services/auth/auth.service';
 import {AuthRequestModel} from '../../../models/auth/auth.request.model';
@@ -8,7 +8,7 @@ import {AuthRequestModel} from '../../../models/auth/auth.request.model';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink],
+  imports: [CommonModule, FormsModule, RouterLink, NgOptimizedImage],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,21 +1,44 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from '../../services/auth/auth.service';
+import {NgClass, NgIf} from '@angular/common';
+import {
+  trigger, state, style, transition, animate
+} from '@angular/animations';
 // import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [
+    NgClass,
+    NgIf,
     // RouterLink
   ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
+  animations: [
+    trigger('fadeSlide', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-10px)' }),
+        animate('200ms ease-out', style({ opacity: 1, transform: 'translateY(0)' })),
+      ]),
+      transition(':leave', [
+        animate('150ms ease-in', style({ opacity: 0, transform: 'translateY(-10px)' }))
+      ]),
+    ]),
+    trigger('rotateIcon', [
+      state('open', style({ transform: 'rotate(180deg)' })),
+      state('closed', style({ transform: 'rotate(0deg)' })),
+      transition('open <=> closed', [
+        animate('200ms ease-in-out')
+      ]),
+    ])
+  ]
 })
 export class HeaderComponent {
   dropdownAtivo = false;
 
-  // constructor(private apiService: ApiService, private router: Router) {
-  //   const isLogado = this.apiService.isAuthenticated();
-  //   console.log('LoginPageComponent: Usuário está logado?', isLogado);
-  // }
+  constructor(private apiService: AuthService, private router: Router) {}
 
   toggleDropdown(event: Event): void {
     event.preventDefault();
@@ -23,17 +46,7 @@ export class HeaderComponent {
   }
 
   logout() {
-    // this.apiService.logout().subscribe({
-    //   next: () => {
-    //     localStorage.removeItem('token');
-    //     this.router.navigate(['/login']);
-    //   },
-    //   error: (err) => {
-    //     console.error('Erro ao fazer logout:', err);
-    //     // mesmo que dê erro, por segurança podemos limpar o token
-    //     localStorage.removeItem('token');
-    //     this.router.navigate(['/login']);
-    //   }
-    // });
+    this.apiService.logout()
+    void this.router.navigate(['/login']);
   }
 }
