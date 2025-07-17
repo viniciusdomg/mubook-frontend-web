fa0d41948fc27d7b39abc4f934e0b4d802db1270
viniciusdomg <viniciusmgomes2005@hotmail.com>
2025-07-17 17:30:08 -0300

    feat: criando componente para agendar reserva

---
+++ src/app/components/reservas-agendar/reservas-agendar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservas-agendar',
+  imports: [],
+  templateUrl: './reservas-agendar.component.html',
+  styleUrl: './reservas-agendar.component.css'
+})
+export class ReservasAgendarComponent {
+
+}
